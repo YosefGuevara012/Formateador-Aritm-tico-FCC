3d5ff974a4cb0196c6d2ab5704db4467c1f80efc
YOSEF GUEVARA <yosef.guevara012@hotmail.com>
2020-08-01 00:01:28 -0500

    se esta dividiendo la informacion correctamente

--- arithmetic_arranger.py
+++ arithmetic_arranger.py
@@ -1,35 +1,52 @@
 def arithmetic_arranger(problems):
     
     if len(problems) > 5:
-      raise TypeError("Too many problems.")
+      raise TypeError("Error: Too many problems.")
     
-    problems_list = []
     num_1=[]
     num_2=[]
     operators=[]
-    result=[]
 
-    for i in range(len(problems)):
+    # spliting the list
 
-      problems_list.append(problems[i].split())
+    for problem in problems:
+      problem = problem.split()
+      num_1.append(problem[0])
+      operators.append(problem[1])
+      num_2.append(problem[2])
 
-      if problems_list[i][1] != '+' and problems_list[i][1]!= "-":
+    # checking the right operators
 
-        raise TypeError("Operator must be '+' or '-'.")
-
-      num_1.append(problems_list[i][0])
-      operators.append(problems_list[i][1])
-      num_2.append(problems_list[i][2])
-
-    result=[]
+    for operator in operators:
+      if operator != "+" and operator != "-":
+        print("Error: Operator must be '+' or '-'.")
     
     for(num1,num2) in zip(num_1,num_2):
-      if len(num1) > 4 or len(num2) > 4:
-        raise TypeError("too long")
+      if num1.isdigit() == False or num2.isdigit() == False:
+        print("Error: Numbers must only contain digits.")
 
-    for i in range(len(problems)):
-      if operators[i] == '+':
-        result[i] = int(num_1[i]) + int(num_2[i])
+    # checking the number of digits
 
-    return problems_list
+    for(num1,num2) in zip(num_1,num_2):
+      if len(num1) > 4  or len(num2) > 4:
+        print("Error: Numbers cannot be more than four digits.")
+    
+
+    result=[]
+
+
+    for(num1,operator,num2) in zip(num_1,operators,num_2):
+      if operator == "+":
+        result.append(str(int(num1)+int(num2)))
+      else:
+        result.append(str(int(num1)-int(num2)))
+    
+
+    all_lists = num_1
+
+    arranged_problems
+
+
+
+    return result[2]
     #return arranged_problems
